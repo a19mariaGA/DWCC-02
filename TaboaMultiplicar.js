49fc238006f85410204d48df37a7ac9f24eba95c
maria 1 <A19MariaGA@iessanclemente.net>
2023-10-06 12:19:53 +0200

    primero

---
+++ TaboaMultiplicar.js
@@ -0,0 +1,13 @@
+
+ function tabaoFor(){
+
+    for (let i = 1; i <= 10; i++) {
+   
+        let resultado = 9 * i;
+
+        console.log(`9 x ${i} = ${resultado}`);
+        
+    
+      }
+
+ }
